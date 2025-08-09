8c17e0bd76224956e88f2aa2ad8ac2a0c5ca0dab
nicostav <104652866+nicostav@users.noreply.github.com>
2025-08-09 10:57:16 +0200

    Update init.lua
    Added relative line numbers

--- init.lua
+++ init.lua
@@ -1014,3 +1014,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Enable relative line numbers
+vim.opt.relativenumber = true
